52ae9f5ca471c120e9d2f3fa9939ccc1cf276ed8
zantgo <100251517+zantgo@users.noreply.github.com>
2026-03-10 02:00:14 -0300

    Bug Fix

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes =[
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/pokedex/features/pokedex/pages/pokemon-list-page/pokemon-list-page').then(m => m.PokemonListPage)
+  },
+  {
+    path: 'pokemon/:id',
+    loadComponent: () =>
+      import('./features/pokedex/features/pokedex/pages/pokemon-detail-page/pokemon-detail-page').then(m => m.PokemonDetailPage)
+  },
+  {
+    path: '**', // Si el usuario escribe una URL que no existe, lo mandamos al inicio
+    redirectTo: ''
+  }
+];
--- frontend/src/app/features/pokedex/components/pokemon-table/pokemon-table.component.ts
+++ frontend/src/app/features/pokedex/components/pokemon-table/pokemon-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { PokedexState } from '../../state/pokedex.state';
 import { KgPipe } from '../../pipes/kg.pipe';
 import { CmPipe } from '../../pipes/cm.pipe';
@@ -8,14 +9,12 @@ import { InvertNamePipe } from '../../pipes/invert-name-pipe';
 @Component({
   selector: 'app-pokemon-table',
   standalone: true,
-  imports: [CommonModule, KgPipe, CmPipe, InvertNamePipe],
+  imports:[CommonModule, RouterModule, KgPipe, CmPipe, InvertNamePipe],
   templateUrl: './pokemon-table.html',
-  styleUrls: ['./pokemon-table.css'],
+  styleUrls:['./pokemon-table.css'],
 })
 export class PokemonTableComponent {
   private state = inject(PokedexState);
-
-  // Estas son las propiedades que tu HTML está buscando
   public readonly pokemons = this.state.pokemonList;
   public readonly isLoading = this.state.isLoading;
   public readonly meta = this.state.currentMetadata;
